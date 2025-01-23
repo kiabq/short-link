75902d4e0a5bce51c6dec7dc659a8e19ee5e3fde
kiabq <conway.joseph@pm.me>
2025-01-22 19:49:18 -0500

    add: postgres connection logic

--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,11 @@
 module kiabq/hyperslice
 
 go 1.23.1
+
+require (
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+)
--- api/internal/server/server.go
+++ api/internal/server/server.go
@@ -1,10 +1,15 @@
 package server
 
 import (
+	"fmt"
 	"kiabq/hyperslice/internal/routes"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
+
+	"github.com/jackc/pgx"
 )
 
 func Start() {
@@ -13,6 +18,23 @@ func Start() {
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
+
+	port, err := strconv.ParseUint(os.Getenv("DATABASE_PORT"), 10, 16)
+
+	if err != nil {
+		// throw error
+	}
+
+	config := pgx.ConnConfig{
+		Host:     os.Getenv("DATABASE_HOST"),
+		Port:     uint16(port),
+		Database: os.Getenv("DATABASE_NAME"),
+		User:     os.Getenv("DATABASE_USER"),
+		Password: os.Getenv("DATABASE_PASS"),
+	}
+
+	conn, err := pgx.Connect(config)
+	fmt.Println(conn)
 
 	routes.RegisterRoutes()
 
