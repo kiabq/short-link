3fcb4755b3c4db6d93a7470f06827488f9a69170
kiabq <conway.joseph@pm.me>
2025-02-19 22:32:08 -0500

    chore: continue migrating ts to go

--- api/internal/db.go
+++ api/internal/db.go
@@ -1,6 +1,12 @@
 package internal
 
-import "github.com/jackc/pgx"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+
+	"github.com/jackc/pgx"
+)
 
 type DatabaseMethods interface {
 	CreateCode()
@@ -12,18 +18,38 @@ type DB struct {
 	connection *pgx.Conn
 }
 
-func (d *DB) CreateCode(url string) {
+func (d *DB) CreateCode(url string) error {
 	var code string = ""
 	chars := []string{"abcdefghijklmnopqrstuvwrxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"}
 
+	for i := 0; i < 6; i++ {
+		char := int(math.Floor(rand.Float64() * float64(len(chars))))
+		code += chars[char]
+	}
+
+	err := d.CheckAlias(code)
+	if err != nil {
+		fmt.Println("error occurred: ", err)
+		return nil
+	}
+
+	fmt.Println("code: ", code)
+
 	// do join to check if code + alias combo exists
-	d.connection.Query(`SELECT * FROM alias WHERE alias = $1`, code)
+	rows, _ := d.connection.Query(``, code)
+	fmt.Println("rows: ", rows)
+
+	return nil
 }
 
-func (d *DB) CheckAlias(code string) {
+func (d *DB) CheckAlias(code string) error {
 	d.connection.Exec(`SELECT * FROM alias WHERE alias = $1`, code)
+
+	return nil
 }
 
-func (d *DB) FetchAlias() {
+func (d *DB) FetchAlias() error {
 	d.connection.Query("")
+
+	return nil
 }
--- api/internal/routes/routes.go
+++ api/internal/routes/routes.go
@@ -1,22 +1,40 @@
 package routes
 
 import (
+	"errors"
 	"fmt"
 	"html"
 	"net/http"
 )
 
+const (
+	INVALID_METHOD string = "Method Not Allowed"
+)
+
 func RegisterRoutes() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Cannot use any other method(s) to access this route
 		if r.Method != "GET" {
-			// Return error
+			err := errors.New(INVALID_METHOD)
+			http.Error(w, err.Error(), 405)
+			return
 		}
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 
 	http.HandleFunc("/{id}", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "POST" {
-			// Return error
+		if r.Method != "POST" || r.Method != "GET" {
+			err := errors.New(INVALID_METHOD)
+			http.Error(w, err.Error(), 405)
+			return
+		}
+
+		if r.Method == "GET" {
+			// Do GET logic
+		}
+
+		if r.Method == "POST" {
+			// Do POST logic
 		}
 
 		id := r.PathValue("id")
